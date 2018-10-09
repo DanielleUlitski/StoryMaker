1e05fd93c8af53e579787e25c846b0722902c2df
unknown <Hyourinmaru80@gmail.com>
2018-10-09 14:34:41 +0300

    added routes

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const router = require('./router');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const app = express();
 const server = app.listen(8000);
 const io = require('socket.io').listen(server);
 
+app.use(express.static(path.join(__dirname, 'client')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router)
+
+const sentence = require('./modules/sentenceModule');
+const user = require('./modules/userModule');
+const story = require('./modules/storyModule');
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+    io.emit('news', 'its a wonderfull day!')
 });
-
-const StoryMaker = mongoose.connect('mongodb://localhost:27017/storymakerdb', (err) => {
-    if (err) console.log(err);
-    else console.log('connection established!!!')
-});
-
-const user = require('./modules/userModule');
-const sentence = require('./modules/sentenceModule');
-const story = require('./modules/storyModule');
-
-app.use(express.static(path.join(__dirname, 'client')))
-app.use(express.static(path.join(__dirname, 'node_modules')))
-
