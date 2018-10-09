75d4004344bb8333bfff171e9935cad18d9d8d0e
unknown <Hyourinmaru80@gmail.com>
2018-10-09 16:02:15 +0300

    added es6 modules

--- client/main.js
+++ client/main.js
@@ -1,2 +1,6 @@
-import Eventhandler from './event-handler'
+import Eventhandler from './event-handler.js'
 
+const eventHandler = new Eventhandler()
+
+eventHandler.socketConnect()
+eventHandler.socketLogin()
