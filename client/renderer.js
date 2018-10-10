2915bd33495a9ddfef41b2364f141bc9b74e8d00
Nitzancohen <nitzancohen105@gmail.com>
2018-10-10 10:08:48 +0300

    added render new story

--- client/renderer.js
+++ client/renderer.js
@@ -3,12 +3,19 @@ class Renderer {
         this.$usersTemplate = $('#users-template').html();
         this.$sentenceTemplate = $('#sentence-template').html();
         this.$storiesTemplate = $('#stories-template').html();
+        this.$newStoryTemplate = $('#new-story-template').html();
     }
 
     renderUsers(users) {
         const template = Handlebars.compile(this.$usersTemplate);
         const newHTML = template({users});
         $('#users-list').empty().append(newHTML);
+    }
+
+    renderNewStory(id) {
+        const template = Handlebars.compile(this.$newStoryTemplate);
+        const newHTML = template({id});
+        $('#main-sceen').empty().append(newHTML);
     }
 
     renderStory(sentences) {
